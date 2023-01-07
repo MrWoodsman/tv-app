5d46234f482bf2937864357af56a0427eb633359
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-01-07 06:39:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+var t
+
+function time() {
+    t = new Date()
+
+    var h, m, s
+    h = t.getHours()
+    m = t.getMinutes()
+    s = t.getSeconds()
+
+    if (h < 10) { h = '0' + h }
+    if (m < 10) { m = '0' + m }
+    if (s < 10) { s = '0' + s }
+
+    const final_time = `${h}:${m}:${s}`
+
+    document.querySelectorAll('.time_js').forEach((e) => {
+        e.innerHTML = final_time
+    })
+}
+time()
+var intetvalTime = setInterval(time,500)

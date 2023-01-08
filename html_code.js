5f1d4ed56c66fbcd171e175b8365780f6e7ac5ca
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-01-08 22:43:11 +0100

    Update html_code.js

--- html_code.js
+++ html_code.js
@@ -1,15 +1,4 @@
 var data = [`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-</head>
-<body>
     <div class="custom_box_1">
         <h1 class="date_js"></h1>
         <h1 class="time_js">00:00:00</h1>
@@ -26,7 +15,4 @@ var data = [`
     </div>
 
     <button id="test_btn" onclick="document.querySelector('#test_btn').innerText = Math.round(Math.random(0,10)*10)">KLIKNIJ MNIE</button>
-</body>
-    <script src="index.js"></script>
-</html>
 `]

80d723b692dd10a37fa2790baf286472ebb00393
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-01-08 04:43:53 +0100

    Update html_code.js

--- html_code.js
+++ html_code.js
@@ -4,4 +4,10 @@ var data = [`
         <h1 class="time_js">00:00:00</h1>
         <!-- <div class="calendar_js"></div> -->
     </div>
+
+    <div class="weather_box">
+        <h1 class="loaction_js"></h1>
+        <h1 class="description_js"></h1>
+        <h1 class="temperature_js"></h1>
+    </div>
 `]

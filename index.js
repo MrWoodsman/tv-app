6f868c68e5465867b9d3ca414c3c970ce580faef
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2023-01-08 02:51:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,31 @@ function time() {
     document.querySelectorAll('.time_js').forEach((e) => {
         e.innerHTML = final_time
     })
+    
+    createCalendar()
 }
 time()
 var intetvalTime = setInterval(time,500)
+
+function createCalendar() {
+    document.querySelector('.calendar_js').innerHTML = ``
+    const weekDay = new Date(t.getFullYear(), t.getMonth(), 1).getDay()
+    //! Tworzenie spacji
+    for (let b = 0; b < 6-weekDay; b++) {
+        const box = document.createElement('p')
+        box.setAttribute('class','empty')
+        document.querySelector('.calendar_js').appendChild(box)
+    }
+    //! Tworzenie dni
+    for (let a = 1; a <= daysInMonth(t.getMonth(), t.getFullYear());  a++) {
+        const box = document.createElement('p')
+        box.setAttribute('class','day')
+        if (t.getDate() == a) { box.classList.add('today')}
+        box.innerHTML = a
+        document.querySelector('.calendar_js').appendChild(box)
+    }
+}
+createCalendar()
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
